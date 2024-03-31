401e98acc53b584e3b5d502cabcba51f1f05ed7b
vanessalsq <leishu_qiu@brown.edu>
2024-03-31 17:37:38 -0400

    march 31

--- Backend/service.py
+++ Backend/service.py
@@ -1,9 +1,17 @@
 # ideal place for the bulk of logic including loading and preprocessing data, and splitting it into chunks.
 import openai
 import os
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 from config import get_api_key
 from data_loader import DataLoader
 from utils import is_pdf_by_extension
+
 
 
 # Assuming you have a text file 'data.txt' in the same directory as your main.py
