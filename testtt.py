8754c3b9303365e9d23acb8a03440bc7ac008d8d
lpplake <lpplake@gmail.com>
2024-04-18 14:17:04 -0400

    updated ./requirements.txt

--- testtt.py
+++
@@ -1,5 +0,0 @@
-texts = ["1", "2", "3", "4", "5"]
-metadatas = [{'source': 'xxx'} for _ in texts]
-metadatas = metadatas or [{} for _ in texts]
-for metadata, text in zip(metadatas, texts):
-    metadata[self._text_key] = text
